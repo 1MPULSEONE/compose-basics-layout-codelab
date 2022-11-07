1e2f39a2472039de3b5fed408e54c6d979916d62
1MPULSEONE <parkhomenko2049@gmail.com>
2022-11-08 00:15:55 +0600

    Small changes

--- app/src/main/java/com/example/basiclayoutcomposecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiclayoutcomposecodelab/MainActivity.kt
@@ -36,12 +36,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicLayoutComposeCodelabTheme {
-                FavoriteCollectionsGrid(
-                    Modifier,
-                    FavoriteCollectionCardRepository().getAllFavoriteCollectionCards()
-                )
-            }
+            BasicLayoutComposeCodelabTheme {}
         }
     }
 
